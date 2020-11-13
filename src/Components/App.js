2899196c612feb41cd2d737f3ccb57cb44c336cc
Fajrul <fajrul452@gmail.com>
2020-11-13 08:27:52 +0700

    AppProvider & AppContext

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import ReactDom from 'react-dom';
 import styled from 'styled-components';
+import {navigate, useRoutes} from 'hookrouter';
+
+import AppProvider from './AppProvider.js';
+import {AppContext} from './AppProvider.js';
 
 const StyledApp = styled.div``;
 
 function App() {
   return (
-    <StyledApp>Heloo world</StyledApp>
+    <AppProvider>
+      <AppContext.Consumer>
+        {({ }) => (
+          <StyledApp>Heloo world</StyledApp>
+        )}
+      </AppContext.Consumer>
+    </AppProvider>
   );
 }
 
---
+++ src/Components/AppProvider.js
@@ -0,0 +1,12 @@
+import React, {useEffect, useState, createContext, useContext} from 'react';
+export const AppContext = createContext();
+
+export default function (props) {
+  return (
+    <AppContext.Provider value={{
+
+    }}>
+      {props.children}
+    </AppContext.Provider>
+  );
+}
